1a0cb24562673a2d2654ab7a6a7f92bfc5fec773
Uncle-Chen <2672818508@qq.com>
2017-05-21 17:13:33 +0800

    waf

--- startup.sh
+++
@@ -1,3 +0,0 @@
-sudo iptables -t nat -A PREROUTING -p tcp -d 127.0.0.1 --dport 80 -m mark ! --mark 100 -j REDIRECT --to-port 3033
-sudo iptables -t nat -A OUTPUT -p tcp -d 127.0.0.1 --dport 80 -m mark ! --mark 100 -j REDIRECT --to-port 3033
-./../redis-3.2.8/src/redis-server
